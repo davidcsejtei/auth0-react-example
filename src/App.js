66f952f39411a405886d78c5d2d0a60c16f3948b
Dávid Csejtei <csejteidavid90@gmail.com>
2021-02-11 09:51:18 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LogInButton from './LogInButton';
+import LogOutButton from './LogOutButton';
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if(isLoading) {
+    return <div>loading...</div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        { !isAuthenticated && <LogInButton /> }
+        { isAuthenticated && <LogOutButton /> }
+        { isAuthenticated && user.email }
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider 
+    domain="dev-cstgs314.eu.auth0.com"
+    clientId="yNmWTv9VrSE9quDeLJqilZ1nARd1mtil"
+    redirectUri={window.location.origin}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
